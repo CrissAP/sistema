b635dd7f66c81fc4e5b0676980ffa71ac3565a32
cristian ancco palomino <CrissAP>
2019-06-24 22:39:46 -0500

    segundo comit

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -1,26 +1,40 @@
 <?php
-
 namespace Tests\Feature;
-
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class UsersModuleTest extends TestCase
 {
-    /** @test*/
+    /** @test */
     function it_loads_the_users_list_page()
     {
         $this->get('/usuarios')
             ->assertStatus(200)
-            ->assertSee('Usuarios');
+            ->assertSee('Listado de usuarios')
+            ->assertSee('Joel')
+            ->assertSee('Ellie');
     }
 
-    /** @test*/
+    /** @test */
+    function it_show_a_default_message_if_the_users_list_is_empty()
+    {
+        $this->get('/usuarios?empty')
+            ->assertStatus(200)
+            ->assertSee('No hay usuarios registrados.');
+    }
+    
+    /** @test */
     function it_loads_the_users_details_page()
     {
         $this->get('/usuarios/5')
             ->assertStatus(200)
-            ->assertSee('Mostrando detalle usuarios: 5');
+            ->assertSee('Mostrando detalle del usuario: 5');
+    }
+    
+    /** @test */
+    function it_loads_the_new_users_page()
+    {
+        $this->get('/usuarios/nuevo')
+            ->assertStatus(200)
+            ->assertSee('Crear nuevo usuario');
     }
 }
